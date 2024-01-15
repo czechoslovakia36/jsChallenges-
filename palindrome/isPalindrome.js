e001b82dd59625b5382eea9696bdce7e68d7a1ca
Vivek Kumar <clocktower77@protonmail.com>
2024-01-15 19:54:50 +0530

    nothing

---
+++ palindrome/isPalindrome.js
@@ -0,0 +1,12 @@
+function isPalindrome2(str){
+
+    revStr='';
+    for(let i=str.length-1;i>=0;i--){
+        // console.log(str[i]);
+        
+    }
+
+}
+
+
+module.exports= isPalindrome2;
---
+++ palindrome/isPalindrome.test.js
@@ -0,0 +1,4 @@
+const isPalindrome2= require('./isPalindrome');
+
+
+isPalindrome2('abcdef');
